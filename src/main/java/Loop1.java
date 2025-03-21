1467cc271a37d273fcb058273d50f6c99f36f843
ashwin zagade <ashwinizagade18@gmail.com>
2025-03-21 11:44:46 +0530

    Adding driver executable

---
+++ src/main/java/Loop1.java
@@ -0,0 +1,12 @@
+public class Loop1 {
+
+    public static void main(String[] args) {
+
+        // defining an object array
+        Object a[] = {"selenium", 1.5, 3 > 12, 'c', 5};
+
+        //print array using for each loop
+        for (Object b : a) ;
+        System.out.println("fffg");
+    }
+}
